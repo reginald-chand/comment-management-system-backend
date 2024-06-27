9e4075e64c6d2547cdbefce7150f483cc9eab7cf
Reginald Chand <reginald-chand@outlook.com>
2024-06-27 23:30:38 +1200

    Create comment.controller.validator.mjs

---
+++ server/validators/comment/comment.controller.validator.mjs
@@ -0,0 +1,14 @@
+import Joi from "joi";
+
+export const commentControllerValidator = Joi.object({
+  _csrf: Joi.string().required(),
+
+  userName: Joi.string().pattern(new RegExp("^[a-z]+$")).required(),
+  userComment: Joi.string().required(),
+
+  postId: Joi.string()
+    .pattern(new RegExp("^(?:[0-9a-fA-F]{24}|(?:[0-9]{1,19}))$"))
+    .required(),
+
+  userData: Joi.object().required(),
+});
